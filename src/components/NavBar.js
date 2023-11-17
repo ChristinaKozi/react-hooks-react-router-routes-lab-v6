b20ec3f8e48b73cc9a5a02ed26c4406238671465
Christina Kozanian <christinakozanian@gmail.com>
2023-11-16 23:50:34 -0800

    lab complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,27 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink
+          to="/"
+          /* add styling to Navlink */
+          className="nav-link"
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/directors"
+          className="nav-link"
+        >
+          Directors
+        </NavLink>
+        
+        <NavLink
+          to="/actors"
+          className="nav-link"
+        >
+          Actors
+        </NavLink>
     </nav>
     );
 };
--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors,setActors] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/actors')
+    .then(r=>r.json())
+    .then(data=>setActors(data))
+    .catch(error => console.error(error))
+  },[])
+
+  const actor = actors.map(actor=>{
+    return (
+      <article key={actor.id}>
+        <h2>{actor.name}</h2>
+        {actor.movies.map(movie=>{
+          return <ul key={movie}>
+            <li>{movie}</li>
+            </ul>
+        })}
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actor}
       </main>
     </>
   );
